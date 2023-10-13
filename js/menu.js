cefa95f0171c03c5e4d0275ae91529b4487093bc
luis Miguel <luism.vieira@iclou.comgit initexitrexitgit config --global user.email luism.vieira@icloud.com>
2023-10-13 16:35:46 -0300

    introdução adicionada

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,17 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
+function toggleMenu(event) {
+    if (event.type == 'touchstart') event.preventDefaut();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTartget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTartget.setAttribute('aria-label', 'Fechar Menu');   
+    } else {
+        event.currentTartget.setAttribute('aria-label', 'Abrir Menu');   
+    }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
